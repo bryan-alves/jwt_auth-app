f53d741ba3883dce05f6ef2ee6b0a6b09be4453f
Bryan Alves <=>
2021-06-07 15:18:09 -0300

    Validação do usuário/senha com a API; Armazenando o token ou apresentando os erros

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-
+import axios from 'axios'
 import Input from '../components/Input'
 import styles from './Login.module.css'
 import background from './Login.jpg';
@@ -8,19 +8,33 @@ import background from './Login.jpg';
 const Login = () => {
 
   const { register, handleSubmit, formState: { errors }, clearErrors } = useForm();
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false)
 
-  const onSubmit = (props) => {
-    console.log(props)
+  const onSubmit = async (form) => {
+    setLoading(true)
+
+    try {
+      const { data } = await axios.post('http://localhost:8000/api/login', form)
+
+      window.localStorage.setItem('token', data.access_token)
+      setError(null)
+    } catch (err) {
+      const { data } = err.response
+      setError(data.error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <div className={styles.login} style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.container}>
+          {error && <p>{error}</p>}
           <Input
             label={'Email'}
             placeholder="usuario@email.com"
-
             {...register("email", {
               required: true, pattern: {
                 value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -28,9 +42,10 @@ const Login = () => {
               }
             })}
             onChange={() => clearErrors("email")}
+            disabled={loading}
           />
-          {errors.email && errors.email.type == 'required' && <p>Preencha este campo</p>}
-          {errors.email && <span role="alert">{errors.email.message}</span>}
+          {errors.email && errors.email.type === 'required' && <p>Preencha este campo</p>}
+          {errors.email && <p>{errors.email.message}</p>}
 
           <Input
             label={'Senha'}
@@ -38,9 +53,13 @@ const Login = () => {
             type={'password'}
             placeholder="******"
             {...register("password", { required: true })}
+            disabled={loading}
           />
-          {errors.password && errors.password.type == 'required' && <p>Preencha este campo</p>}
-          <button type="submit" className={styles.button}>Acessar</button>
+          {errors.password && errors.password.type === 'required' && <p>Preencha este campo</p>}
+
+          <button type="submit" className={styles.button} disabled={loading}>
+            {!loading ? 'Acessar' : 'Aguarde...'}
+          </button>
         </div>
       </form>
     </div>
